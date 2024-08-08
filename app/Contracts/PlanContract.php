6288b87bf360b56c0930ccff93639fff51124754
joaomgsa <joaomgsa@gmail.com>
2024-08-08 18:44:29 -0300

    Undone contracts

---
+++ app/Contracts/PlanContract.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Contracts;
+
+use Illuminate\Support\Collection;
+
+interface PlanContract
+{
+
+
+    public function listPlans(string $order = 'id', string $sort = 'desc', array $columns = ['*']): Collection;
+    public function findPlanById(int $id): Plan;
+
+    public function createPlan(array $params): Plan;
+
+    public function updatePlan(array $params): Plan;
+
+    public function deletePlan($id): bool;
+}
